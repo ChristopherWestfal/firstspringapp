f50d5ae8cbd4657d0841771c47bf464937e63e3d
ChrizDope <incubusid@googlemail.com>
2024-06-17 12:45:06 +0200

    Added nearly all Tasks

--- src/main/java/org/example/firstspringapp/repository/CharacterRepo.java
+++ src/main/java/org/example/firstspringapp/repository/CharacterRepo.java
@@ -2,9 +2,11 @@ package org.example.firstspringapp.repository;
 
 import org.example.firstspringapp.model.AsterixCharacter;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CharacterRepo extends MongoRepository<AsterixCharacter, String> {
     List<AsterixCharacter> findAllByName(String name);
     List<AsterixCharacter> findAllByAge(int age);

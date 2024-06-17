16b5acaaff093ed1ba14782ff3d895d073deeb9b
ChrizDope <incubusid@googlemail.com>
2024-06-17 10:46:16 +0200

    Initial commit

---
+++ src/test/java/org/example/firstspringapp/FirstspringappApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.firstspringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstspringappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
